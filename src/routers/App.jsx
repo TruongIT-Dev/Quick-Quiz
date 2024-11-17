f1e37a1c211cff59645dbc6a8779188af8c688d0
Truong-Nguyen <dangtruongnguyen2@gmail.com>
2024-11-17 23:06:33 +0700

    fix

--- src/routers/App.jsx
+++ src/routers/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom"
 
 import Home from "../pages/Home"
-import Quiz from "../pages/Quiz"
 import NotObeyRule from "../pages/Rule"
 import EndingPage from "../pages/EndPage"
+import QuizPage from "../pages/Quiz"
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/quiz" element={<QuizPage />} />
         <Route path="/not-obey-rule" element={<NotObeyRule />} />
         <Route path="/ending" element={<EndingPage />} />
       </Routes>
